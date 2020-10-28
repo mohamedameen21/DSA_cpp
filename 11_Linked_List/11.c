a719bf1617b7e0a9c3694c4b15c7023e90b3a530
mohamedameen21 <mohamedameen.21.2001@gmail.com>
2020-10-28 10:37:23 +0530

    Finally I found the extension of executable file in linux

---
+++ 11_Linked_List/11.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main()
+{
+    int a = 20;
+    printf("Hello World \n I am Ameen ");
+    return 0;
+}
+
+// Hello I  think it is cool How are you I am struggling to complete this full screen sorry baby Have you had your Breakfast I had 
