7c21a46f9a8aed53760115f132135ac31dd2e5bb
mohamedameen21 <mohamedameen.21.2001@gmail.com>
2021-01-23 10:21:25 +0530

    Heap Finished

---
+++ 19_Heap/2_Priority_Queue.c
@@ -0,0 +1,5 @@
+//Priority Queue is that deleting the higher priority first
+
+//We can use heap to implement priority queue It is much 
+//faster than array The operations takes less time than the 
+//operations performed in array..........
